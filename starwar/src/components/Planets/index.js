f6c310abe56162f1bc4b6cf9aba5a26e9d1cfb59
saurabh21shukla <saurabh21shukla@gmail.com>
2020-02-21 20:36:10 +0530

    Remove starwars folder from main

--- starwar/src/components/Planets/index.js
+++
@@ -1,4 +0,0 @@
-import {PlanetsList} from './PlanetsList';
-import {PlanetsView} from './PlanetsView';
-
-export default {PlanetsList, PlanetsView};
--- starwar/src/store/actions/userLogin.js
+++
@@ -1,9 +0,0 @@
-export const USER_LOGIN = 'USER_LOGIN';
-export const USER_LOGIN_AUTHENTICATED = 'USER_LOGIN_AUTHENTICATED';
-export const USER_LOGIN_ERROR = 'USER_LOGIN_ERROR';
-export const USER_CREATE_ERROR = 'USER_CREATE_ERROR';
-
-export const userLogin = ({username, password, history}) => ({
-    type: USER_LOGIN,
-    payload: {username, password, history},
-});
